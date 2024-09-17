7e866261248a6e9883b1d52b69d5c0cee5392b56
Mori Hiroshi <mori.hiroshi.a.c@gmail.com>
2024-09-16 23:27:18 -0400

    fluxo entre cenas

--- Unity_TopDown/Assets/Scripts/Menu/MenuPrincipalManager.cs
+++ Unity_TopDown/Assets/Scripts/Menu/MenuPrincipalManager.cs
@@ -1,32 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class MenuPrincipalManager : MonoBehaviour
+
+namespace Menu
 {
-    [SerializeField] private string nomeDoLevelDeJogo;
-    [SerializeField] private GameObject painelMenuInicial;
-    [SerializeField] private GameObject painelOpcoes;
-    public void Jogar()
+    public class MenuPrincipalManager : MonoBehaviour
     {
-        SceneManager.LoadScene("");
-    }
+        [SerializeField] private string nomeDoLevelDeJogo;
+        [SerializeField] private GameObject painelMenuInicial;
+        [SerializeField] private GameObject painelOpcoes;
+        public void Jogar()
+        {
+            SceneManager.LoadScene(nomeDoLevelDeJogo);
+        }
 
-    public void AbrirOpcoes()
-    {
-        painelMenuInicial.SetActive(false);
-        painelOpcoes.SetActive(true);
-    }
+        public void AbrirOpcoes()
+        {
+            painelMenuInicial.SetActive(false);
+            painelOpcoes.SetActive(true);
+        }
 
-    public void FecharOpcoes()
-    {
-        painelOpcoes.SetActive(false);
-        painelMenuInicial.SetActive(true);
-    }
+        public void FecharOpcoes()
+        {
+            painelOpcoes.SetActive(false);
+            painelMenuInicial.SetActive(true);
+        }
 
-    public void SairJogo()
-    {
-        Debug.Log("Sair do jogo");
-        Application.Quit();
+        public void SairJogo()
+        {
+            Debug.Log("Sair do jogo");
+            Application.Quit();
+        }
     }
 }
