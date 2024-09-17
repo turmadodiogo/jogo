d0617ae57df96dca0fa066eba5b32eac67038f5d
Mori Hiroshi <mori.hiroshi.a.c@gmail.com>
2024-09-16 22:28:26 -0400

    subindo primeiro menu

---
+++ Unity_TopDown/Assets/Scripts/Menu/MenuPrincipalManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuPrincipalManager : MonoBehaviour
+{
+    [SerializeField] private string nomeDoLevelDeJogo;
+    [SerializeField] private GameObject painelMenuInicial;
+    [SerializeField] private GameObject painelOpcoes;
+    public void Jogar()
+    {
+        SceneManager.LoadScene("");
+    }
+
+    public void AbrirOpcoes()
+    {
+        painelMenuInicial.SetActive(false);
+        painelOpcoes.SetActive(true);
+    }
+
+    public void FecharOpcoes()
+    {
+        painelOpcoes.SetActive(false);
+        painelMenuInicial.SetActive(true);
+    }
+
+    public void SairJogo()
+    {
+        Debug.Log("Sair do jogo");
+        Application.Quit();
+    }
+}
